adaade2d0c7846ffa541b21b71d04df1ed496c58
Niklas Witt <niklas.witt@nord-com.net>
2020-09-16 12:37:34 +0200

    Add changed kpath file(s)
    - Changed kpath.py to fit to hdf5 file format
    - Added main_kpath.py to exclusevely extract kpath after finished calculation

--- multi_orbital/evaluation/kpath_extract.py
+++ multi_orbital/evaluation/kpath_extract.py
@@ -1,7 +1,7 @@
 ## -*- encoding: latin-1 -*-
 
 from numpy import *
-
+import h5py
     
 class kpath_extract:
     def __init__(self,p,h,b,g):
@@ -9,62 +9,61 @@ class kpath_extract:
         ### G function
         k_HSP, gkio_HSP = kpath_extract.kpath_extractor(p,\
                             (trace(g.gkio[b.f_iwn_zero_ind],0,1,2)/3).reshape(p.nk1,p.nk2,p.nk3))
-        kpath_extract.kpath_save_data(k_HSP, gkio_HSP, p.kpath_savepath.format("gkio"))
 
         ### max eigenvalue spin susceptibility        
-        E_  = tensordot(ones(len(b.bm)), array([eye(p.nwan**2,p.nwan**2) for it in range(p.nk)]), axes=0)
-        chi_spin   = g.ckio@linalg.inv(E_ - g.ckio@h.S_mat)
+        chi_spin   = g.ckio@linalg.inv(g.E_int - g.ckio@h.S_mat)
         chi_s = chi_spin[b.b_iwn_zero_ind].reshape(p.nk1,p.nk2,p.nk3,p.nwan**2,p.nwan**2)
         chi_s_eig, _ = linalg.eigh(chi_s)
+        _, chi_s_HSP = kpath_extract.kpath_extractor(p, (chi_s_eig[:,:,:,-1]))
         
-        k_HSP, chi_s_HSP = kpath_extract.kpath_extractor(p, (chi_s_eig[:,:,:,-1]))
-        kpath_extract.kpath_save_data(k_HSP, chi_s_HSP, p.kpath_savepath.format("chi_s_maxeig"))       
-        
+        ### max eigenvalue charge susceptibility
+        chi_charge = g.ckio@linalg.inv(g.E_int + g.ckio@h.C_mat)
+        chi_c = chi_charge[b.b_iwn_zero_ind].reshape(p.nk1,p.nk2,p.nk3,p.nwan**2,p.nwan**2)
+        chi_c_eig, _ = linalg.eigh(chi_c)
+        _, chi_c_HSP = kpath_extract.kpath_extractor(p, (chi_c_eig[:,:,:,-1]))
+
 # =============================================================================
 #         ### gap function
-#         k_HSP, gap_HSP = kpath_extract.kpath_extractor(p,\
+#         _ gap_HSP = kpath_extract.kpath_extractor(p,\
 #                             (trace(el.delta[b.f_iwn_zero_ind],0,1,2)/3).reshape(p.nk1,p.nk2,p.nk3))
-#         kpath_extract.kpath_save_data(k_HSP, gap_HSP, p.kpath_savepath.format("gap_"+p.SC_type+"w"))
 # =============================================================================
-        
+             
+        with h5py.File(savepath,'a') as file:
+            group = file.require_group('kpath_KGMK')
+    
+            group.require_dataset('kvalue',data=k_HSP,shape=k_HSP.shape,dtype=k_HSP.dtype)
+            group.require_dataset('chi_spin_max_eig',data=chi_s_HSP,shape=chi_s_HSP.shape,dtype=chi_s_HSP.dtype)
+            group.require_dataset('chi_charge_max_eig',data=chi_c_HSP,shape=chi_c_HSP.shape,dtype=chi_c_HSP.dtype)        
+            group.require_dataset('gkio_trace',data=gkio_HSP,shape=gkio_HSP.shape,dtype=gkio_HSP.dtype)
+            #group.require_dataset('gap_{}'.format(p.SC_type),data=gap_HSP,shape=gap_HSP.shape,dtype=gap_HSP.dtype)
+      
         
     def kpath_extractor(p, quant):
         """
         Extracts points of given quantity along HSP k-path  K->G->M->K.
         """
         ##### Path extraction
+        k_HSP_pos = array([0, 4/3, 4/3+2/sqrt(3), 4/3+2/sqrt(3)+2/3])/(4/3+2/3+2/sqrt(3))
+        
         ### K->Gamma [Gamma -> K]
-        k_HSP_GK  = linspace(0,1.057,p.nk2//3+1)
-        quant_HSP_GK = zeros((p.nk2//3+1),dtype='complex')
-        for it in range(p.nk2//3+1):
+        k_HSP_GK  = linspace(k_HSP_pos[0],k_HSP_pos[1],p.nk1//3+1) 
+        quant_HSP_GK = zeros((p.nk1//3+1),dtype='complex')
+        for it in range(p.nk1//3+1):
             quant_HSP_GK[it] = quant[it,it,0]
         quant_HSP_GK = quant_HSP_GK[::-1]
-
-        ### K -> M [K ->M]
-        k_HSP_KM  = linspace(1.972,2.5,p.nk2//6+1)
-        quant_HSP_KM = zeros((p.nk2//6+1),dtype='complex')
-        for it in range(p.nk2//6+1):
-            quant_HSP_KM[it] = quant[p.nk2//2-it,2*it,0]
-
+        
         ### M -> Gamma [Gamma -> M]
-        k_HSP_MG  = linspace(1.057,1.972,p.nk2//2+1)
+        k_HSP_MG  = linspace(k_HSP_pos[1],k_HSP_pos[2],p.nk1//2+1)
         quant_HSP_MG = quant[0:p.nk1//2+1,0,0]
         
-        # Extract along HSP k-line:
-        k_HSP  = array([k_HSP_GK, k_HSP_MG, k_HSP_KM])
-        quant_HSP = array([quant_HSP_GK, quant_HSP_MG, quant_HSP_KM])
-        #k_HSP_pos = array([0, 1.057, 1.972, 2.5])
-        return k_HSP, quant_HSP
-    
-    def kpath_save_data(k, quant, path):
+        ### K -> M [K ->M]
+        k_HSP_KM  = linspace(k_HSP_pos[2],k_HSP_pos[3],p.nk1//6+1)
+        quant_HSP_KM = zeros((p.nk1//6+1),dtype='complex')
+        for it in range(p.nk1//6+1):
+            quant_HSP_KM[it] = quant[p.nk1//2-it,2*it,0]
         
-        file = open(path, "w")
-        for it_path in range(3):
-            q_shape = k[it_path].shape
-            for it_k in range(q_shape[0]):
-                file.write(str(k[it_path][it_k]) +\
-                    " " + str(real(quant[it_path][it_k])) +\
-                    " " + str(imag(quant[it_path][it_k])) + "\n")
-            file.write("\n")
-        file.close()
+        # Extract along HSP k-line:
+        k_HSP     = concatenate(array([k_HSP_GK, k_HSP_MG, k_HSP_KM]))
+        quant_HSP = concatenate(array([quant_HSP_GK, quant_HSP_MG, quant_HSP_KM]))
 
+        return k_HSP, quant_HSP
---
+++ multi_orbital/evaluation/main_kpath.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 20:45:04 2020
+
+@author: nikla
+"""
+
+import sys
+
+MKL_THREADS_VAR = str(sys.argv[1])
+
+import os
+os.environ["MKL_NUM_THREADS"] = MKL_THREADS_VAR
+os.environ["NUMEXPR_NUM_THREADS"] = MKL_THREADS_VAR
+os.environ["OMP_NUM_THREADS"] = "1"
+
+from numpy import *
+from einsum2 import einsum2
+import scipy as sc
+import pyfftw
+from ir_load import ir_load
+from parameters import parameters
+from hamiltonian import hamiltonian
+from gfunction import gfunction_load
+from kpath_extract import kpath_extract
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
+import time
+
+
+#### For converison now
+import h5py
+sp_name = "NaxCoO2_calculation_data_T_{}_U_{}_JUratio_{}_n_{}.h5"
+
+##### Please input in order: 
+# MKL_NUM_THREADS | T | T_load | JUratio | JU_ratio_load | round_it
+n_fill = (7-3.43)/2
+T      = float(sys.argv[2])
+T_load = float(sys.argv[3])
+JU_ratio = float(sys.argv[4])
+JU_ratio_load = float(sys.argv[5])
+
+round_it = int(sys.argv[6])
+
+print(T, T_load, JU_ratio, JU_ratio_load, round_it)
+
+
+### Initiate parameters -------------------------------------------------
+start = time.process_time()
+p = parameters(round(T, 5), round(n_fill ,5), round(JU_ratio,5), round_it,\
+               T_load = round(T_load, 5), JU_ratio_load = round(JU_ratio_load, 5))
+print("##################################################"\
+      , file=open(p.Logstr,'a'))
+print("Extract kpath of gkio,chi_s,chi_c:"\
+      , file=open(p.Logstr,'a'))
+print(datetime.datetime.now().strftime('%d. %B %Y %I:%M%p')\
+      , file=open(p.Logstr,'a'))
+print("Parameter set: n = {}, T = {}, U = {}, J/U = {}\n".format\
+     (p.n_fill, p.T, p.u0 ,p.JU_ratio), file=open(p.Logstr,'a'))
+print("Elapsed time - parameter init: " + str(time.process_time() - start)\
+      , file=open(p.Logstr,'a')) 
+
+### Load hamiltionian----------------------------------------------------            
+t_hset = time.process_time()
+h = hamiltonian(p)
+print("Elapsed time - hamiltonian set (tot | module): " \
+      + str(time.process_time() - start) + " | " \
+      + str(time.process_time() - t_hset), file=open(p.Logstr,'a'))
+    
+    
+### Load irbasis --------------------------------------------------------            
+t_bload = time.process_time()
+b = ir_load(p.Lambda, p.beta)
+print("Elapsed time - basis load (tot | module): " \
+      + str(time.process_time() - start) + " | " \
+      + str(time.process_time() - t_bload), file=open(p.Logstr,'a')) 
+
+
+### Calculate full Greens function---------------------------------------
+print("Load gkio data.", file=open(p.Logstr,'a')) 
+g = gfunction_load(p, b)
+print("Elapsed time - data load (tot | module): " \
+      + str(time.process_time() - start) + " | " \
+      + str(time.process_time() - t_gcal), file=open(p.Logstr,'a')) 
+
+### Use kpath extract for G, chi_s,  chi_x ------------------------------
+print("Calculate and save kpath.", file=open(p.Logstr,'a')) 
+kpath_extract(p, h, b, g)    
+    
+print('Finished current parameters. Move to next or quit.', file=open(p.Logstr,'a'))
+print('#########################################################', file=open(p.Logstr,'a'))     
